ec3127996be8e56fe56cf3f5dae69aeadbb99387
kip <kip@airnavindonesia.co.id>
2025-05-18 03:27:06 +0000

    Update on Mei

--- wp-config.php
+++ wp-config.php
@@ -86,7 +86,7 @@ $table_prefix = 'wp_';
 define('WP_DEBUG', true);
 define('WP_DEBUG_LOG', true);
 define('WP_DEBUG_DISPLAY', false);
-define('WP_MEMORY_LIMIT', '256M');
+define('WP_MEMORY_LIMIT', '512M');
 define('WP_MAX_MEMORY_LIMIT', '512M');
 define('DISABLE_WP_CRON', true);
 
